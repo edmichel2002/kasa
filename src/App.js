dbeb74e7de695f201ede70f855a837ed25443706
edmichel2002 <eddymichel2002@gmail.com>
2023-12-11 07:52:35 +0100

    modifs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from "react";
-/*import './App.css';*/
-
 import Header from './Components/Header/Header';
-import Footer from './Components/Footer/Footer';
-
-//import Banner from './components/Banner/Banner';
-/*import Card from './components/Card/Card';*/
-/*import Main from  './components/Main/Main';*/
-/*import Slideshow './components/Slideshow/Slideshow';*/
-/*import Collapse from './components/Collapse/Collapse';*/
-
+import Footer from './Components/Footer/Footer'
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import NoPage from './pages/NoPage/NoPage';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Accomodation from './pages/Accomodation/Accomodation';
-
 
 function App() {
   return (
